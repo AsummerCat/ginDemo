60e7361613f1624f706453e4c37d5b6e28a20935
chenxiaocheng <583188551@qq.com>
2022-10-18 11:40:22 +0800

    获取表单参数 和url参数 及其api参数

--- src/main.go
+++ src/main.go
@@ -12,16 +12,38 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		//可通过contxt.Query获取带参数的路由
 		param := c.Query("userName")
+		//可通过contxt.DefaultQuery如果无参数则给默认值
+		param1 := c.DefaultQuery("userName", "默认值")
 		//返回浏览器 状态码,输出
 		c.String(http.StatusOK, "获取参数:"+param+"\n")
+		c.String(http.StatusOK, "获取参数:"+param1+"\n")
 		c.String(http.StatusOK, "Hello！欢迎来到GO世界！\n")
 		c.String(http.StatusOK, "请求路径:"+c.Request.URL.Path+"\n")
 		c.String(http.StatusOK, "请求ip:"+c.ClientIP())
 	})
+
+	router.GET("/:name", func(c *gin.Context) {
+		//可通过contxt.Query获取带参数的路由
+		param := c.Param("name")
+		//返回浏览器 状态码,输出
+		c.String(http.StatusOK, "获取参数:"+param+"\n")
+	})
+
+	router.POST("/testPost", testPost)
+
 	//绑定路由规则 不加函数,表示空白页
-	router.POST("/xxxPost")
 	router.GET("/xxxPost")
 	router.PUT("/xxx")
 	// 3.默认端口是8080,也可以指定端口 r.Run(":80")
 	router.Run()
+}
+
+/*
+*
+获取表单参数
+*/
+func testPost(c *gin.Context) {
+	//获取表单参数
+	form := c.PostForm("name")
+	c.String(http.StatusOK, "获取表单参数:"+form)
 }
