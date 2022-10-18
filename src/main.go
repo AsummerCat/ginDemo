4104ea67c05ce1cdfeeaa3be0bef5b1ba457d399
chenxiaocheng <583188551@qq.com>
2022-10-18 11:28:53 +0800

    路径包含参数获取

--- src/main.go
+++ src/main.go
@@ -10,7 +10,13 @@ func main() {
 	router := gin.Default()
 	//2.绑定路由规则,执行函数
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello！欢迎来到GO世界！")
+		//可通过contxt.Query获取带参数的路由
+		param := c.Query("userName")
+		//返回浏览器 状态码,输出
+		c.String(http.StatusOK, "获取参数:"+param+"\n")
+		c.String(http.StatusOK, "Hello！欢迎来到GO世界！\n")
+		c.String(http.StatusOK, "请求路径:"+c.Request.URL.Path+"\n")
+		c.String(http.StatusOK, "请求ip:"+c.ClientIP())
 	})
 	//绑定路由规则 不加函数,表示空白页
 	router.POST("/xxxPost")
